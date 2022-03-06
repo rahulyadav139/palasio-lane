72975becd5bfe4422e07e908ec62267a57d597de
rahulyadav139 <ry.rahulmyadav@gmail.com>
2022-03-06 23:23:13 +0530

    docs: login modal added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import LoginModal from './components/LoginModal';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <LoginModal />
     </div>
   );
 }
