e532ff15a4bf817739a2c9d7faf02bc6fdc4e6cf
rahulyadav139 <ry.rahulmyadav@gmail.com>
2022-03-16 14:44:24 +0530

    docs: custom hook is created for taking user input

---
+++ src/hooks/use-input.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+const useInput = validateFun => {
+  const [value, setValue] = useState('');
+  const [isTouched, setIsTouched] = useState(false);
+
+  const isValid = validateFun(value);
+  const isInvalid = !validateFun(value) && isTouched;
+
+  const changeHandler = e => {
+    setValue(e.target.value);
+  };
+
+  const blurHandler = () => {
+    setIsTouched(true);
+  };
+
+  return {
+    value,
+    isTouched,
+    setIsTouched,
+    isValid,
+    isInvalid,
+    changeHandler,
+    blurHandler,
+  };
+};
+
+export { useInput };
